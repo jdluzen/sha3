f310eb07f80d047cc1001945b5a03357cd165157
Joe Dluzen <jdluzen@gmail.com>
2013-09-02 15:15:17 -0400

    Add ExtremelyLong tests

--- SHA3Tests/Program.cs
+++ SHA3Tests/Program.cs
@@ -12,6 +12,13 @@ using PetaTest;
 
 namespace SHA3Tests
 {
+    enum TestType
+    {
+        Short,
+        Long,
+        ExtremelyLong
+    }
+
     [TestFixture]
     class Program
     {
@@ -20,25 +27,37 @@ namespace SHA3Tests
             new PetaTest.Runner().Run(args);
         }
 
-        string fileFormat = "{0}MsgKAT_{1}.txt.gz";
-        string[] GetLines(bool isShort, int hashLen)
+        static string fileFormat = "{0}MsgKAT_{1}.txt.gz";
+        string[] GetLines(TestType type, int hashLen)
         {
-            using (GZipStream g = new GZipStream(File.OpenRead(Path.Combine("..", Path.Combine("binaries", string.Format(fileFormat, isShort ? "Short" : "Long", hashLen)))), CompressionMode.Decompress))
+            using (GZipStream g = new GZipStream(File.OpenRead(Path.Combine("..", Path.Combine("binaries", string.Format(fileFormat, type, hashLen)))), CompressionMode.Decompress))
             using (StreamReader reader = new StreamReader(g))
                 return reader.ReadToEnd().Split('\r', '\n');
         }
 
-        IEnumerable<Tuple<int, byte[], string>> GetDataAndHash(string[] lines)
+        IEnumerable<Tuple<int, byte[], string>> GetDataAndHash(TestType type, string[] lines)
         {
-            for (int i = 0; i < lines.Length; i++)
+            switch (type)
             {
-                if (!lines[i].StartsWith("#") && lines[i] != string.Empty)
-                {
-                    int len = int.Parse(lines[i].Split(' ')[2]);
-                    if (len % 8 == 0)//only on byte ranges
-                        yield return new Tuple<int, byte[], string>(len / 8, lines[i + 1].Split(' ')[2].FromHexString(), lines[i + 2].Split(' ')[2]);
-                    i += 2;
-                }
+                case TestType.Short:
+                case TestType.Long:
+                    for (int i = 0; i < lines.Length; i++)
+                        if (!lines[i].StartsWith("#") && lines[i] != string.Empty)
+                        {
+                            int len = int.Parse(lines[i].Split(' ')[2]);
+                            if (len % 8 == 0)//only on whole byte ranges
+                                yield return new Tuple<int, byte[], string>(len / 8, lines[i + 1].Split(' ')[2].FromHexString(), lines[i + 2].Split(' ')[2]);
+                            i += 2;
+                        }
+                    break;
+                case TestType.ExtremelyLong:
+                    for (int i = 0; i < lines.Length; i++)
+                        if (!lines[i].StartsWith("#") && lines[i] != string.Empty)
+                        {
+                            yield return new Tuple<int, byte[], string>(int.Parse(lines[i].Split(' ')[2]), ASCIIEncoding.ASCII.GetBytes(lines[i + 1].Split(' ')[2]), lines[i + 2].Split(' ')[2]);
+                            i += 2;
+                        }
+                    break;
             }
         }
 
@@ -58,10 +77,10 @@ namespace SHA3Tests
             yield return new SHA3Portable::SHA3.SHA3Managed(hashLen);
         }
 
-        void RunTest(bool isShort, int hashLen)
+        void RunTest(TestType type, int hashLen)
         {
-            string[] lines = GetLines(isShort, hashLen);
-            foreach (Tuple<int, byte[], string> tup in GetDataAndHash(lines))
+            string[] lines = GetLines(type, hashLen);
+            foreach (Tuple<int, byte[], string> tup in GetDataAndHash(type, lines))
             {
                 foreach (object impl in GetImplementations(hashLen))
                 {
@@ -88,9 +107,11 @@ namespace SHA3Tests
                             };
                         init = (impl as SHA3Portable::SHA3.IHashAlgorithm).Initialize;
                     }
-                    if (tup.Item1 > 0)
-                        for (int i = 0; i < tup.Item2.Length; i++)//really stress the internal buffer
-                            transformBlock(tup.Item2, i, 1, tup.Item2, i);
+                    int repeats = type == TestType.ExtremelyLong ? tup.Item1 : 1;
+                    if (tup.Item1 > 0)//only on whole byte ranges
+                        for (int r = 0; r < repeats; r++)
+                            for (int i = 0; i < tup.Item2.Length; i++)//really stress the internal buffer
+                                transformBlock(tup.Item2, i, 1, tup.Item2, i);
                     byte[] hash = transformFinalBlock(tup.Item2, 0, 0);
                     string shash = BitConverter.ToString(hash).Replace("-", string.Empty);
                     if (shash != tup.Item3)
@@ -99,7 +120,8 @@ namespace SHA3Tests
 
                     init();
                     if (tup.Item1 > 0)
-                        transformBlock(tup.Item2, 0, tup.Item1, tup.Item2, 0);
+                        for (int r = 0; r < repeats; r++)
+                            transformBlock(tup.Item2, 0, tup.Item2.Length, tup.Item2, 0);//transform in one shot
                     hash = transformFinalBlock(tup.Item2, 0, 0);
                     shash = BitConverter.ToString(hash).Replace("-", string.Empty);
                     if (shash != tup.Item3)
@@ -112,49 +134,73 @@ namespace SHA3Tests
         [Test]
         public void Short_224()
         {
-            RunTest(true, 224);
+            RunTest(TestType.Short, 224);
         }
 
         [Test]
         public void Short_256()
         {
-            RunTest(true, 256);
+            RunTest(TestType.Short, 256);
         }
 
         [Test]
         public void Short_384()
         {
-            RunTest(true, 384);
+            RunTest(TestType.Short, 384);
         }
 
         [Test]
         public void Short_512()
         {
-            RunTest(true, 512);
+            RunTest(TestType.Short, 512);
         }
 
         [Test]
         public void Long_224()
         {
-            RunTest(false, 224);
+            RunTest(TestType.Long, 224);
         }
 
         [Test]
         public void Long_256()
         {
-            RunTest(false, 256);
+            RunTest(TestType.Long, 256);
         }
 
         [Test]
         public void Long_384()
         {
-            RunTest(false, 384);
+            RunTest(TestType.Long, 384);
         }
 
         [Test]
         public void Long_512()
         {
-            RunTest(false, 512);
+            RunTest(TestType.Long, 512);
+        }
+
+        [Test]
+        public void ExtremelyLong_224()
+        {
+            RunTest(TestType.ExtremelyLong, 224);
+        }
+
+        [Test]
+        public void ExtremelyLong_256()
+        {
+            RunTest(TestType.ExtremelyLong, 256);
+        }
+
+        [Test]
+        public void ExtremelyLong_384()
+        {
+            RunTest(TestType.ExtremelyLong, 384);
+        }
+
+        [Test]
+        public void ExtremelyLong_512()
+        {
+            RunTest(TestType.ExtremelyLong, 512);
         }
     }
 }
